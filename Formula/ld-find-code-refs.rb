7baab17c9926ba66d83b4900e77b54ae6320c1b0
Arnold Trakhtenberg <arnold@launchdarkly.com>
2019-01-24 14:07:37 -0800

    Update ld-find-code-refs.rb

--- Formula/ld-find-code-refs.rb
+++ Formula/ld-find-code-refs.rb
@@ -3,8 +3,8 @@
 class LdFindCodeRefs < Formula
   desc "Enables you to keep track of feature flag usage in your code from LaunchDarkly."
   homepage "https://launchdarkly.com/"
-  url "https://github.com/launchdarkly/ld-find-code-refs/releases/download/0.3.0/ld-find-code-refs_0.3.0_linux_amd64.tar.gz"
-  sha256 "ba44359a90e9f1baceabdf25fd8000a3472a36ef5da44e102bf5523281cb4183"
+  url "https://github.com/launchdarkly/ld-find-code-refs/releases/download/0.3.0/ld-find-code-refs_0.3.0_darwin_amd64.tar.gz"
+  sha256 "1f010cb846ed26af60efdc937297ef4b346326d6342c22a5798acd6d1226c03c"
   version "0.3.0"
 
   depends_on "ag"

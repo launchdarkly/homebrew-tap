b45a13b6105f5d43e43b4d7ef203475424b745a9
goreleaserbot <dev@launchdarkly.com>
2019-02-01 11:58:25 -0800

    Brew formula update for ld-find-code-refs version 0.5.0-rc1

--- Formula/ld-find-code-refs.rb
+++ Formula/ld-find-code-refs.rb
@@ -1,16 +1,13 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
 class LdFindCodeRefs < Formula
-  desc "Enables you to keep track of feature flag usage in your code from LaunchDarkly."
-  homepage "https://launchdarkly.com/"
-  url "https://github.com/launchdarkly/ld-find-code-refs/releases/download/0.4.0/ld-find-code-refs_0.4.0_darwin_amd64.tar.gz"
-  sha256 "0d482581f9f40f6e94a4ce0a43b3c5a46050587af88c834671c35aca653d58c1"
-  version "0.4.0"
-
+  desc "Job for finding and sending feature flag code references to LaunchDarkly"
+  homepage "https://launchdarkly.com"
+  url "http://github.com/launchdarkly/ld-find-code-refs/releases/download/0.5.0-rc1/ld-find-code-refs_0.5.0-rc1_darwin_amd64.tar.gz"
+  version "0.5.0-rc1"
+  sha256 "49ef63c8ce97c48b21d17986c1e484d2db1794acf384e5a363714018b39be4cd"
+  
   depends_on "ag"
 
   def install
-    # links the binary into /usr/local/bin
     bin.install "ld-find-code-refs"
   end
 end

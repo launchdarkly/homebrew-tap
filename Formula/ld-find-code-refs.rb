962eca65397980616623d2d1c001b5cf80ecacd6
LaunchDarklyCI <dev@launchdarkly.com>
2019-09-19 14:56:51 -0700

    Brew formula update for ld-find-code-refs version 1.3.0

--- Formula/ld-find-code-refs.rb
+++ Formula/ld-find-code-refs.rb
@@ -2,16 +2,16 @@
 class LdFindCodeRefs < Formula
   desc "Job for finding and sending feature flag code references to LaunchDarkly"
   homepage "https://launchdarkly.com"
-  version "1.2.0"
+  version "1.3.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/launchdarkly/ld-find-code-refs/releases/download/1.2.0/ld-find-code-refs_1.2.0_darwin_amd64.tar.gz"
-    sha256 "784a278138193b20c6ce296d4750f89dc58ab8bc292dd3f513bedfc250bd0ad7"
+    url "https://github.com/launchdarkly/ld-find-code-refs/releases/download/1.3.0/ld-find-code-refs_1.3.0_darwin_amd64.tar.gz"
+    sha256 "6295258cd6bf5d33326f1722939c5b05fcd352097f6ff4ecfb3d087a7d434089"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/launchdarkly/ld-find-code-refs/releases/download/1.2.0/ld-find-code-refs_1.2.0_linux_amd64.tar.gz"
-      sha256 "ac0b851b7613e34ffe1db98da724f8ece674836482358a5d7743a747dd8e2c52"
+      url "https://github.com/launchdarkly/ld-find-code-refs/releases/download/1.3.0/ld-find-code-refs_1.3.0_linux_amd64.tar.gz"
+      sha256 "bf54106457d3add098b7d919f06d99a7505868b7da6a371b9124ad052febd29f"
     end
   end
   
